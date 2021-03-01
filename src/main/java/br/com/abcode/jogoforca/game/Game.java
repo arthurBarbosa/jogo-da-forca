4bc7765e891c2fb7ea438b4224c0c65f54c3538f
Arthur Barbosa <arthurbruno03@gmail.com>
2021-03-01 00:02:31 -0300

    finalizado classe Game

--- src/main/java/br/com/abcode/jogoforca/game/Game.java
+++ src/main/java/br/com/abcode/jogoforca/game/Game.java
@@ -1,13 +1,70 @@
 package br.com.abcode.jogoforca.game;
 
 import br.com.abcode.jogoforca.core.Dictionary;
+import br.com.abcode.jogoforca.core.InvalidCharacterException;
 import br.com.abcode.jogoforca.core.Word;
+import br.com.abcode.jogoforca.ui.UI;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Game {
 
+    private static final int MAX_ERRORS = 5;
+
     public void start() {
-        Dictionary d = Dictionary.getInstance();
-        Word w = d.nextWorld();
-        System.out.println(w.getOriginalWord());
+        UI.print("Bem vindo ao jogo da Forca!");
+
+        Dictionary dictionary = Dictionary.getInstance();
+        Word word = dictionary.nextWorld();
+
+        UI.print("A palavra tem " + word.size() + " letras");
+
+        Set<Character> usedChars = new HashSet<>();
+        int errorCount = 0;
+
+        while (true) {
+            UI.print(word);
+            UI.printNewLine();
+
+            char c;
+            try {
+                c = UI.readChar("Digite uma letra");
+
+                if (usedChars.contains(c)) {
+                    throw new InvalidCharacterException("Esta letra já foi utilizada");
+                }
+
+                usedChars.add(c);
+
+                if (word.hasChar(c)) {
+                    UI.print("Você acertou uma letra!");
+                } else {
+                    errorCount++;
+
+                    if (errorCount < MAX_ERRORS) {
+                        UI.print("Você errou! Você ainda pode errar " + (MAX_ERRORS - errorCount) + " vez(es)");
+                    }
+                }
+                UI.printNewLine();
+
+                if (word.discovered()) {
+                    UI.print("Parabéns! Você acertou a palavra correta: " + word.getOriginalWord());
+                    UI.print("FIM DE JOGO!");
+                    break;
+                }
+
+                if (errorCount == MAX_ERRORS) {
+                    UI.print("Você perdeu o jogo! A palavra correta era: " + word.getOriginalWord());
+                    UI.print("FIM DE JOGO!");
+                    break;
+                }
+
+            } catch (InvalidCharacterException e) {
+                UI.print("Erro: " + e.getMessage());
+                UI.printNewLine();
+            }
+        }
+
     }
 }
