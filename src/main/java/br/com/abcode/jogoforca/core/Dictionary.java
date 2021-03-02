79ad72901bd7f94aaa43aecb8af07abf30ec093b
Arthur Barbosa <arthurbruno03@gmail.com>
2021-03-01 23:39:53 -0300

    Creating multiple dictionaries

--- src/main/java/br/com/abcode/jogoforca/core/Dictionary.java
+++ src/main/java/br/com/abcode/jogoforca/core/Dictionary.java
@@ -1,48 +1,17 @@
 package br.com.abcode.jogoforca.core;
 
-import br.com.abcode.jogoforca.exception.GameException;
-import br.com.abcode.jogoforca.utils.RandomUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-public class Dictionary {
-
-    private static final String FILE_NAME = "dicionario.txt";
+public abstract class Dictionary {
 
     private static Dictionary instance;
 
-    private List<String> words = new ArrayList<>();
-
-    private Dictionary() {
-        load();
-    }
-
     public static Dictionary getInstance() {
         if (instance == null) {
-            instance = new Dictionary();
+            instance = new StaticDictionary();
         }
         return instance;
     }
 
-    private void load() {
-        try (Scanner scanner =
-                     new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
+    public abstract Word nextWorld();
 
-            while (scanner.hasNextLine()) {
-                String word = scanner.nextLine().trim();
-                words.add(word);
-            }
-
-            if (words.size() == 0) {
-                throw new GameException("A lista de palavra não pode ser vazia.");
-            }
-        }
-    }
-
-    public Word nextWorld() {
-        int pos = RandomUtils.newRandomNumber(0, words.size());
-        return new Word(words.get(pos));
-    }
+    public abstract String getName();
 }
