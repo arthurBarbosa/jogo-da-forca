da74d3fcd9edd21805b8e3198af70ff60717ef5f
Arthur Barbosa <arthurbruno03@gmail.com>
2021-03-01 23:57:46 -0300

    Choosing dictionary to be used

--- src/main/java/br/com/abcode/jogoforca/core/Dictionary.java
+++ src/main/java/br/com/abcode/jogoforca/core/Dictionary.java
@@ -1,12 +1,23 @@
 package br.com.abcode.jogoforca.core;
 
+import java.lang.reflect.Constructor;
+
 public abstract class Dictionary {
 
     private static Dictionary instance;
 
     public static Dictionary getInstance() {
         if (instance == null) {
-            instance = new StaticDictionary();
+            try {
+                String dictionaryClassName = Config.get("dictionaryClassName");
+                Class<?> clazz = Class.forName(dictionaryClassName);
+                Constructor<?> constructor = clazz.getConstructor();
+                instance = (Dictionary) constructor.newInstance();
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
         }
         return instance;
     }
